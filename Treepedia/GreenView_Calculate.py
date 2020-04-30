15ed7409432c160f687b97cc5ee1ee19b2f143e7
Sau Yee Chan <15137352+y26805@users.noreply.github.com>
2020-04-30 19:27:38 +0900

    Use key in URL generation

--- Treepedia/GreenView_Calculate.py
+++ Treepedia/GreenView_Calculate.py
@@ -267,7 +267,8 @@ def GreenViewComputing_ogr_6Horizon(GSVinfoFolder, outTXTRoot, greenmonth, key_f
                         print("Heading is: ",heading)
                         
                         # using different keys for different process, each key can only request 25,000 imgs every 24 hours
-                        URL = "http://maps.googleapis.com/maps/api/streetview?size=400x400&pano=%s&fov=60&heading=%d&pitch=%d&sensor=false&key=AIzaSyAwLr6Oz0omObrCJ4n6lI4VbCCvmaL1Z3Y"%(panoID,heading,pitch)
+                        URL = "http://maps.googleapis.com/maps/api/streetview?size=400x400&pano=%s&fov=60&heading=%d&pitch=%d&sensor=false&key=%s"%(panoID,heading,pitch, key)
+                        print(URL)
                         # let the code to pause by 1s, in order to not go over data limitation of Google quota
                         time.sleep(1)
                         
