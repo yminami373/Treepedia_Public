d65b6c3cc7cb1e5f6fa461afba64fd25b9ba63c1
Sau Yee Chan <15137352+y26805@users.noreply.github.com>
2020-06-24 17:37:36 +0900

    Add year to arg

--- tests/test_metadataCollector.py
+++ tests/test_metadataCollector.py
@@ -144,7 +144,7 @@ class TestMetadataCollector(unittest.TestCase):
         # case 1: pano is next pano (second in list)
         greenmonth = ["07", "08"]
         year = 2014
-        actual = metadataCollector.get_next_pano_in_greenmonth(pano_list, greenmonth)
+        actual = metadataCollector.get_next_pano_in_greenmonth(pano_list, greenmonth, year)
         expected_panoDate = "2014-07"
         expected_panoId = 'DWd8HToF5fZMV7Fjnh3COw'
         expected_panoLat = 42.37278743408569
@@ -158,7 +158,7 @@ class TestMetadataCollector(unittest.TestCase):
         # case 2: pano is third in list as second pano is not in greenmonth
         greenmonth = ["08"]
         year = 2014
-        actual = metadataCollector.get_next_pano_in_greenmonth(pano_list, greenmonth)
+        actual = metadataCollector.get_next_pano_in_greenmonth(pano_list, greenmonth, year)
         expected_panoDate = "2011-08"
         expected_panoId = 'kFEbzO11yCR-XrQKEjM87g'
         expected_panoLat = 42.37279881264624
@@ -172,7 +172,7 @@ class TestMetadataCollector(unittest.TestCase):
         # case 3: no other panos in greenmonth
         greenmonth = ["04"]
         year = 2014
-        actual = metadataCollector.get_next_pano_in_greenmonth(pano_list, greenmonth)
+        actual = metadataCollector.get_next_pano_in_greenmonth(pano_list, greenmonth, year)
         expected_panoDate = "2018-09"
         expected_panoId = 'P32D4tE8PDMCwXwquHDbFA'
         expected_panoLat = 42.3727815168744
