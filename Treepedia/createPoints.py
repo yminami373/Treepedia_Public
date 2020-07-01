caaebe6cb4dbd0fd9873dd580817c5d5bbd337f2
Sau Yee Chan <15137352+y26805@users.noreply.github.com>
2020-07-01 22:40:45 +0900

    Remove return statement so programme does not terminate

--- Treepedia/createPoints.py
+++ Treepedia/createPoints.py
@@ -97,7 +97,6 @@ def createPoints(inshp, outshp, mini_dist):
                 except:
                     print("You should make sure the input shapefile is WGS84")
                     print(sys.exc_info())
-                    return
                     
     print("Process Complete")
     
